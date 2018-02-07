d03611b65f99f8c6b7445b37dd797b3211a68577
asgari <asgari@inside.sahab.ir>
2018-02-07 21:32:08 +0330

    Add nginx load balancer to docker-compose

--- deployments/build.sh
+++ deployments/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# finch-REST
 go build -o finch-rest/finch ../
 cp ../configs/finch.yml finch-rest/
 cd finch-rest & docker build --tag finch-rest:latest ./finch-rest
+
+# finch load balancer
+cd lb & docker build --tag finch-lb:latest ./lb
